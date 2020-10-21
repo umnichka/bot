4bb4116ed6c9f4771f240abf56428d6051fb2461
umnichka <umnichka@users.noreply.github.com>
2020-10-21 18:02:25 +0600

    upd

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,17 @@
+const Discord = require('discord.js');
 const fs = require('fs')
 
 module.exports = {
 	name: 'help',
 	description: 'List all available commands.',
-	async run (message) {
+	async run (client, message) {
 		let str = '';
 		const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 		for (const file of commandFiles) {
 			const command = require(`./${file}`);
-			str += `Name: ${command.name}, Description: ${command.description} \n`;
-		}
-
-		message.channel.send(str);
-	},
+            str += `Name: ${command.name}, Description: ${command.description} \n`;
+        }
+        await message.reply(str)
+	}
 };
